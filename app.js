734a4793af4a7a043be3f47a512903f9dcd59249
Neil Campbell <neil@codecise.com>
2014-06-22 22:35:53 +1000

    Small refactoring and adding mkdir if directory does not exist

--- app.js
+++ app.js
@@ -3,49 +3,65 @@ var fs = require('fs');
 var shortId = require('shortid');
 
 var options = {
-	clientId: '',
-	tag: ''
+    clientId: '',
+    tag: ''
 };
 
-var instaExport = function(options) {
-	if(options.clientId === '' || options.tag === '') {
-		throw new Error('Please supply both clientId and tag');
-	}
+var InstaExport = (function () {
 
-	var tagExportUrl = 'https://api.instagram.com/v1/tags/' + options.tag + '/media/recent?client_id=' + options.clientId;
-	
-	function fetchImages(url) {
-		request(url, function (error, response, body) {
-			if(error || response.statusCode !== 200) {
-				console.log('Error getting Instagram images');
-				return;
-			}
-			
-			var response = JSON.parse(body);
-			var nextUrl = response.pagination.next_url;
-			
-			for(var i = 0; i < response.data.length; i++) {
-				var item = response.data[i];
-				console.log('exporting: ' + item.images.standard_resolution.url);
-				downloadImage(item.images.standard_resolution.url);
-			}
-			if(nextUrl) {
-				fetchImages(nextUrl);
-			}
-			else {
-				console.log('done');
-			}
-		});
-	}
+    var exportUrlFormat = 'https://api.instagram.com/v1/tags/' + options.tag + '/media/recent?client_id=' + options.clientId;
 
-	function downloadImage(url) {
-		request(url).pipe(fs.createWriteStream('images/' + shortId.generate() + '.jpg'))
-	}
-	
-	return {
-		downloadImages: function() { fetchImages(tagExportUrl); }
-	};
-};
+    function InstaExport(options) {
+        if (options.clientId === '' || options.tag === '') {
+            throw new Error('Please supply both clientId and tag');
+        }
+    }
 
-var exporter = instaExport(options);
+    function createDirectoryIfItDoesntExist(path) {
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
+        }
+    }
+
+    function downloadImage(url, exportPath) {
+        createDirectoryIfItDoesntExist(exportPath);
+        request(url).pipe(fs.createWriteStream(exportPath + '/' + shortId.generate() + '.jpg'));
+    }
+
+    function fetchAndDownloadImages(url, exportPath) {
+        request(url, function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                console.log('Error getting Instagram images');
+                return;
+            }
+
+            var response = JSON.parse(body);
+            var nextUrl = response.pagination.next_url;
+
+            for (var i = 0; i < response.data.length; i++) {
+                var item = response.data[i];
+                console.log('exporting: ' + item.images.standard_resolution.url);
+                downloadImage(item.images.standard_resolution.url, exportPath);
+            }
+            if (nextUrl) {
+                fetchAndDownloadImages(nextUrl, exportPath);
+            }
+            else {
+                console.log('done');
+            }
+        });
+    }
+
+    InstaExport.prototype.downloadImages = function (exportPath) {
+        if (!exportPath) {
+            exportPath = "images/"
+        }
+
+        fetchAndDownloadImages(exportUrlFormat, exportPath);
+    };
+
+    return InstaExport;
+})();
+
+var exporter = new InstaExport(options);
 exporter.downloadImages();
